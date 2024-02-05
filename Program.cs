104afeaf3d198886af03bea686ea8f9420dbbcbf
Stanislav <stanich81@gmail.com>
2024-02-06 00:44:25 +0300

    Добавлено решение ко 2й задаче

--- Program.cs
+++ Program.cs
@@ -27,26 +27,40 @@
 // Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
 // Даны два неотрицательных числа m и n.
 
+// Console.Write("Введите значение M: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите значение N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// int ShowAkkerman(int m, int n)
+// {
+//     if (m == 0) return n + 1;
+//     else if (n == 0) return ShowAkkerman(m - 1, 1);
+//     else return ShowAkkerman(m - 1, ShowAkkerman(m, n - 1));
+// }
+
+// Console.Write($"Функция Аккермана равно { ShowAkkerman(m, n)} ");
+
 
 // Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. 
 // Использовать рекурсию, не использовать циклы.
 
 // ________________________________________________________________________
 
-void ShowArray(int[] arr, int size)
-{
-    int j = size - 1;
-    if (j == 0)
-    {
-        Console.Write(arr[j]);
-        return;
-    }
-    Console.Write($"{arr[j]} ");
-    ShowArray(arr, size-1);
-}
+// void ShowArray(int[] arr, int size)
+// {
+//     int j = size - 1;
+//     if (j == 0)
+//     {
+//         Console.Write(arr[j]);
+//         return;
+//     }
+//     Console.Write($"{arr[j]} ");
+//     ShowArray(arr, size-1);
+// }
 
-int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
-int size = array.Length;
-ShowArray(array, size);
+// int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+// int size = array.Length;
+// ShowArray(array, size);
 
 // ________________________________________________________________________
